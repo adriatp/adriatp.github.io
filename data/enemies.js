72e96e43f66f80c7b64b1171d3dd6eed28390034
adriatp <tarradas.adria@gmail.com>
2022-04-20 21:54:06 +0200

    Add viz 2

---
+++ data/enemies.js
@@ -0,0 +1,126 @@
+country_enemies = {
+    "nodes": [
+      {
+        "id": 1,
+        "name": "Afghan"
+      },
+      {
+        "id": 2,
+        "name": "China"
+      },
+      {
+        "id": 5,
+        "name": "France"
+      },
+      {
+        "id": 6,
+        "name": "Germany"
+      },
+      {
+        "id": 7,
+        "name": "India"
+      },
+      {
+        "id": 9,
+        "name": "Iraq"
+      },
+      {
+        "id": 10,
+        "name": "Isis"
+      },
+      {
+        "id": 11,
+        "name": "Pakistan"
+      },
+      {
+        "id": 12,
+        "name": "Qatar"
+      },
+      {
+        "id": 13,
+        "name": "Russia"
+      },
+      {
+        "id": 14,
+        "name": "Syria"
+      },
+      {
+        "id": 15,
+        "name": "Taliban"
+      },
+      {
+        "id": 16,
+        "name": "UK"
+      },
+      {
+        "id": 17,
+        "name": "USA"
+      }
+    ],
+    "links": [
+      {
+        "source": 1,
+        "target": 10
+      },
+      {
+        "source": 1,
+        "target": 15
+      },
+      {
+        "source": 1,
+        "target": 10
+      },
+      {
+        "source": 5,
+        "target": 10
+      },
+      {
+        "source": 5,
+        "target": 14
+      },
+      {
+        "source": 6,
+        "target": 14
+      },
+      {
+        "source": 7,
+        "target": 15
+      },
+      {
+        "source": 9,
+        "target": 10
+      },
+      {
+        "source": 9,
+        "target": 12
+      },
+      {
+        "source": 10,
+        "target": 14
+      },
+      {
+        "source": 10,
+        "target": 12
+      },
+      {
+        "source": 10,
+        "target": 16
+      },
+      {
+        "source": 10,
+        "target": 17
+      },
+      {
+        "source": 12,
+        "target": 14
+      },
+      {
+        "source": 14,
+        "target": 16
+      },
+      {
+        "source": 14,
+        "target": 17
+      },
+    ]
+  }
