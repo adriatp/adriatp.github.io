6d562e9de1bc0e61cd1ea99f86e27838eb1a4fa2
adriatp <tarradas.adria@gmail.com>
2022-04-20 21:53:54 +0200

    Add viz 1

---
+++ assets/js/viz_1.js
@@ -0,0 +1,86 @@
+window.chartColors = {
+	green: '#75c181',
+	gray: '#a9b5c9',
+	text: '#252930',
+	border: '#e7e9ed'
+};
+
+var lineChartConfig = {
+	type: 'line',
+
+	data: {
+        
+        labels: erto_listed["Data"],
+		
+		datasets: [{
+                label: "Nombre d'expedients",
+                borderDash: [3, 5],
+                fill: false,
+                backgroundColor: window.chartColors.green,
+                borderColor: window.chartColors.green,
+                data: erto_listed["Nombre d'expedients"],
+            },
+            {
+                label: "Nombre d'afectats",
+                fill: false,
+                backgroundColor: window.chartColors.gray,
+                borderColor: window.chartColors.gray,
+                data: erto_listed["Nombre d'afectats"],
+            }]
+	},
+	options: {
+		responsive: true,		
+		
+		legend: {
+			display: true,
+			position: 'bottom',
+			align: 'end',
+		},
+
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+			titleMarginBottom: 10,
+			bodySpacing: 10,
+			xPadding: 16,
+			yPadding: 16,
+			borderColor: window.chartColors.border,
+			borderWidth: 1,
+			backgroundColor: '#fff',
+			bodyFontColor: window.chartColors.text,
+			titleFontColor: window.chartColors.text
+		},
+		hover: {
+			mode: 'nearest',
+			intersect: true
+		},
+		scales: {
+			xAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+				scaleLabel: {
+					display: false,
+				
+				}
+			}],
+			yAxes: [{
+				display: true,
+				gridLines: {
+					drawBorder: false,
+					color: window.chartColors.border,
+				},
+				scaleLabel: {
+					display: false,
+				},
+				ticks: {
+		            beginAtZero: true
+		        },
+			}]
+		}
+	}
+};
+
+new Chart(document.getElementById('canvas_viz_1'), lineChartConfig);
